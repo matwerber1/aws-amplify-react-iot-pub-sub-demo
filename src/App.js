b2de23ea1d26c33b42dcd7768f989d86cb07d2ec
Mat Werber <matwerber@gmail.com>
2019-02-12 20:56:24 -0500

    import amplify & auth components

--- src/App.js
+++ src/App.js
@@ -2,6 +2,11 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import Amplify from 'aws-amplify';
+import awsmobile from './aws-exports';
+import { withAuthenticator } from 'aws-amplify-react';
+Amplify.configure(awsmobile);
+
 class App extends Component {
   render() {
     return (
