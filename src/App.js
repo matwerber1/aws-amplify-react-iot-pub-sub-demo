3f02b3f52fb5342f86c15b0070914f85c58045bd
Mat Werber <matwerber@gmail.com>
2019-02-18 01:12:23 -0500

    add react-json-view

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import './App.css';
 import Amplify from 'aws-amplify';
 import awsmobile from './aws-exports';
 import { withAuthenticator } from 'aws-amplify-react';
+import ReactJson from 'react-json-view';
 Amplify.configure(awsmobile);
 
 class App extends Component {
@@ -13,18 +14,40 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
         </header>
+        <AuthDataDisplay {...this.props} />
+      </div>
+    );
+  }
+}
+
+class AuthDataDisplay extends Component {
+
+  render() {
+
+    var keysToCollapse = ['client','refreshToken'];
+
+    return (
+      <div className="AuthDataDisplay">
+          <p>
+          Auth state is {this.props.authState}
+          </p>
+          Auth data is <br/>
+          <ReactJson 
+            src={this.props.authData} 
+            theme={"hopscotch"}
+            displayObjectSize={false} 
+            displayDataTypes={false} 
+            collapseStringsAfterLength={50} 
+            name={false}
+            shouldCollapse={(field)=>{
+              if (keysToCollapse.includes(field.name)) {
+                return true;
+              } else {
+                return false;
+              }
+            }}
+          />
       </div>
     );
   }
