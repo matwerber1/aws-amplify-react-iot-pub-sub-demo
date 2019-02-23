2a26e8aaa420f6fafb1fc570bec70390c2b41a09
Mat Werber <matwerber@gmail.com>
2019-02-23 07:38:50 -0500

    rewrite to use React Hooks instead of Components

--- src/App.js
+++ src/App.js
@@ -1,46 +1,42 @@
 import React, { Component, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
-
-import Amplify from 'aws-amplify';
+import Amplify, { Auth } from 'aws-amplify';
 import awsmobile from './aws-exports';
 import { withAuthenticator } from 'aws-amplify-react';
-
 import JSONTree from 'react-json-tree';
-
 Amplify.configure(awsmobile);
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-        </header>
-        <br/>
-        <AuthDisplay {...this.props} />
-      </div>
-    );
-  }
+function App(props) {
+  
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+      </header>
+      <br/>
+      <AuthDisplay {...props} />
+    </div>
+  );
+
 }
 
-class AuthDisplay extends Component {
+function AuthDisplay(props) {
 
-  render() {
-
-    var keysToCollapse = ['client','refreshToken'];
+  const [authState, setAuthState] = useState(props.authState);
+  const [authData, setAuthData] = useState(props.authData);
 
     return (
       <div className="AuthDisplay">
-          Auth state: {this.props.authState} <br/> <br/>
-          
-          Auth data:<br/>
-          <div className="AuthData">
-            <JSONTree data={this.props.authData} theme={"tomorrow"} invertTheme={true} />
-          </div>
-      </div>
-    );
-  }
+        Auth state: {authState} <br/> <br/>
+      
+        Auth data:<br/>
+        <div className="AuthData">
+          <JSONTree data={authData} theme={"tomorrow"} invertTheme={true} />
+        </div>
+    </div>
+  );
+
 }
 
 export default withAuthenticator(App, true);
