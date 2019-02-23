c34b2682d9eb8f20493fb435a5d9739be513ce52
Mat Werber <matwerber@gmail.com>
2019-02-23 07:40:37 -0500

    remove unused hooks from authDisplay

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
-import React, { Component, useState } from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import Amplify, { Auth } from 'aws-amplify';
+import Amplify from 'aws-amplify';
 import awsmobile from './aws-exports';
 import { withAuthenticator } from 'aws-amplify-react';
 import JSONTree from 'react-json-tree';
@@ -23,16 +23,13 @@ function App(props) {
 
 function AuthDisplay(props) {
 
-  const [authState, setAuthState] = useState(props.authState);
-  const [authData, setAuthData] = useState(props.authData);
-
     return (
       <div className="AuthDisplay">
-        Auth state: {authState} <br/> <br/>
+        Auth state: {props.authState} <br/> <br/>
       
         Auth data:<br/>
         <div className="AuthData">
-          <JSONTree data={authData} theme={"tomorrow"} invertTheme={true} />
+          <JSONTree data={props.authData} theme={"tomorrow"} invertTheme={true} />
         </div>
     </div>
   );
